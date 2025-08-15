df246460bac6714a9a3cd928e582bb35c90e2872
Leap <git@leap.new>
2025-08-15 17:15:18 +0000

    Manual code change

--- backend/dashboard/db.ts
+++ backend/dashboard/db.ts
@@ -1,6 +1,6 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 
 // Ganti nama resource DB agar unik (hindari bentrok sebelumnya)
-export const dashboardDB = new SQLDatabase("dashboard_v2", {
+export const dashboardDB = new SQLDatabase("dashboard_v3", {
   migrations: "./migrations",
 });
